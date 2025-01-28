b23e77b2efe0258a30ff3d17ec082320ad40659d
Duc Truong <minhduc9111999@gmail.com>
2025-01-28 17:05:43 +0700

    threesum

--- go/solutions/15_3sum/3sum.go
+++ go/solutions/15_3sum/3sum.go
@@ -1,5 +1,7 @@
 package threesum
 
+import "sort"
+
 // Title: 3Sum
 
 // Problem link: https://leetcode.com/problems/3sum
@@ -10,5 +12,35 @@ package threesum
 // case 2: [0,0,0]
 
 func threeSum(nums []int) [][]int {
-    
-} 
+	sort.Ints(nums)
+	result := [][]int{}
+	for i, v := range nums {
+		if i != 0 && v == nums[i-1] {
+			continue
+		}
+		l, r := i+1, len(nums)-1
+		for l < r {
+			if l-1 != i && nums[l] == nums[l-1] {
+				l++
+				continue
+			}
+			if r != len(nums)-1 && nums[r] == nums[r+1] {
+				r--
+				continue
+			}
+			if nums[l]+nums[r] == -v {
+				result = append(result, []int{v, nums[l], nums[r]})
+				l++
+				r--
+				continue
+			}
+			if nums[l]+nums[r] < -v {
+				l++
+				continue
+			}
+			r--
+			continue
+		}
+	}
+	return result
+}
--- go/solutions/15_3sum/3sum_test.go
+++ go/solutions/15_3sum/3sum_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 )
 
+// case 0: [-1,0,1,2,-1,-4]
+// case 1: [0,1,1]
+// case 2: [0,0,0]
 func Test_threeSum(t *testing.T) {
 	type args struct {
 		nums []int
@@ -15,6 +18,34 @@ func Test_threeSum(t *testing.T) {
 		want [][]int
 	}{
 		// TODO: Add test cases.
+		{
+			name: "Test 1",
+			args: args{
+				nums: []int{-1, 0, 1, 2, -1, -4},
+			},
+			want: [][]int{{-1, -1, 2}, {-1, 0, 1}},
+		},
+		{
+			name: "test 2",
+			args: args{
+				nums: []int{0, 1, 1},
+			},
+			want: [][]int{},
+		},
+		{
+			name: "Test 3",
+			args: args{
+				nums: []int{0, 0, 0},
+			},
+			want: [][]int{{0, 0, 0}},
+		},
+		{
+			name: "Test 4",
+			args: args{
+				nums: []int{-2, 0, 0, 2, 2},
+			},
+			want: [][]int{{-2, 0, 2}},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
